d82652f6e9e137c463b1c9ef79eff0939d871791
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-11-26 16:41:10 +0100

    ✨ Rental page

---
+++ src/Containers/Rental/Rental.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Rental extends Component {
+ render() {
+  return (
+   <div>
+    <h1>C'est l'appartement</h1>
+   </div>
+  )
+ }
+}
