3334b8adf3c1a1438df3e501fb5423c2f0b769b8
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-11-26 16:40:49 +0100

    🎨 Create error page

---
+++ src/Containers/Errors/Error.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Error extends Component {
+	render() {
+		return (
+			<div>
+				<h1>C'est l'erreur !!</h1>
+			</div>
+		);
+	}
+}
