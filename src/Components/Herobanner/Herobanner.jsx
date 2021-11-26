9bd655660829804fbbd6f2ba04302910c81f9a68
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-11-26 16:37:42 +0100

    ✨ Create herobanner component

---
+++ src/Components/Herobanner/Herobanner.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import './Herobanner.scss'
+
+export default class Herobanner extends Component {
+	render() {
+		return (
+			<div className="herobanner">
+				<h1 className="herobanner__title">Chez vous, partout et ailleurs</h1>
+			</div>
+		);
+	}
+}
