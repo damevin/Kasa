1d27d3a708644aad78a09ad42395f7994f6ab94f
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-11-30 19:03:03 +0100

    ✨  Make cards clickable for been redirected to rental page

--- src/Components/RentalCard/RentalCard.jsx
+++ src/Components/RentalCard/RentalCard.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./RentalCard.scss";
 
 export default class RentalCard extends Component {
 	render() {
-		function test() {
-			console.log("okok");
-		};
 
 		return (
-			<article className="rentalcard" onClick={test}>
-				<div className="rentalcard__overlay"></div>
-				<img className="rentalcard__img" src={this.props.cover} alt=""></img>
-				<h2 className="rentalcard__title">{this.props.title}</h2>s
-			</article>
+			<Link to={"/rental/" + this.props.id}>
+				<article className="rentalcard">
+					<div className="rentalcard__overlay"></div>
+					<img className="rentalcard__img" src={this.props.cover} alt=""></img>
+					<h2 className="rentalcard__title">{this.props.title}</h2>s
+				</article>
+			</Link>
 		);
 	}
 }
