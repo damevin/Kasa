73e338f7eaaff3975a0687620aca12ea8b33c16f
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-11-30 19:03:39 +0100

    🚧  Get rental id on his page for fetching data

--- src/Containers/Rental/Rental.jsx
+++ src/Containers/Rental/Rental.jsx
@@ -1,11 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import data from "../../data/data.json";
 
 export default class Rental extends Component {
- render() {
-  return (
-   <div>
-    <h1>C'est l'appartement</h1>
-   </div>
-  )
- }
+	render() {
+		const rentalData = data;
+		const rentalUrlId = window.location.pathname.substr(8);
+		console.log(rentalData);
+
+		const rental = rentalData.find((item) => item.id === rentalUrlId);
+		console.log(rental);
+		return <div>{rental.title}</div>;
+	}
 }
