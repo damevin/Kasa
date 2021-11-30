3ab572cefe1cbfe8622fe3582dc4e47984f02252
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-11-30 19:32:46 +0100

    🚨  Fix linter errors

--- src/Containers/Rental/Rental.jsx
+++ src/Containers/Rental/Rental.jsx
@@ -5,10 +5,8 @@ export default class Rental extends Component {
 	render() {
 		const rentalData = data;
 		const rentalUrlId = window.location.pathname.substr(8);
-		console.log(rentalData);
-
 		const rental = rentalData.find((item) => item.id === rentalUrlId);
-		console.log(rental);
+  
 		return <div>{rental.title}</div>;
 	}
 }
