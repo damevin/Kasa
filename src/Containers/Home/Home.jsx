a6418e8967921313b78ac5cc3874331362608b49
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-11-30 19:03:22 +0100

    🗃️  Fetch real data instead fake data

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,37 +1,18 @@
 import React, { Component } from "react";
 import Herobanner from "../../Components/Herobanner/Herobanner";
 import RentalCard from "../../Components/RentalCard/RentalCard";
-
+import data from "../../data/data.json";
 import { useState } from "react";
 
 export default class Home extends Component {
 	render() {
-		const dataArr = [
-			{
-				title: "Titre de la location",
-				cover:
-					"https://image.shutterstock.com/image-photo/british-shorthair-kitten-silver-color-260nw-1510641710.jpg",
-				id: 23982,
-			},
-			{
-				title: "Test",
-				cover:
-					"https://image.shutterstock.com/image-photo/british-shorthair-kitten-silver-color-260nw-1510641710.jpg",
-				id: 23982,
-			},
-			{
-				title: "Test",
-				cover:
-					"https://image.shutterstock.com/image-photo/british-shorthair-kitten-silver-color-260nw-1510641710.jpg",
-				id: 23982,
-			},
-		];
+		const dataArr = data;
 		return (
 			<main>
 				<Herobanner></Herobanner>
 				<section class="rental__section">
 					{dataArr.map((item, index) => {
-						return <RentalCard title={item.title} cover={item.cover} key={index}></RentalCard>;
+						return <RentalCard title={item.title} cover={item.cover} key={index} id={item.id}></RentalCard>;
 					})}
 				</section>
 			</main>
