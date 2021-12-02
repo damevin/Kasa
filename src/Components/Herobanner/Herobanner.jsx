19309bd6f3c1a015d43626788758b63c87c37520
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-02 18:46:26 +0100

    🐛  Update herobanner for two different options

--- src/Components/Herobanner/Herobanner.jsx
+++ src/Components/Herobanner/Herobanner.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
-import './Herobanner.scss'
+import "./Herobanner.scss";
 
 export default class Herobanner extends Component {
 	render() {
-		return (
-			<div className="herobanner">
-				<h1 className="herobanner__title">Chez vous, partout et ailleurs</h1>
-			</div>
-		);
+		if (this.props.origin === "homepage") {
+			return (
+				<div className="herobanner">
+					<h1 className="herobanner__title">Chez vous, partout et ailleurs</h1>
+				</div>
+			);
+		} else if (this.props.origin === "about") {
+			return (
+				<div className="herobanner__about">
+				</div>
+			)
+		} 
 	}
 }
