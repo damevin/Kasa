4482dc96536a5cb5f3b09d6ebaafbded8c2ff36a
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-02 13:45:12 +0100

    🚧  Collapse in progress

---
+++ src/Components/Collapse/Collapse.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import Chevron from "./chevron.svg";
+import "./Collapse.scss";
+
+export default class Collapse extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { isOpenned: false };
+	}
+
+	toggleCollapse() {
+		this.setState({
+			isOpenned: !this.state.isOpenned,
+		});
+	}
+
+
+	collapseContent() {
+		const content = this.props.content;
+
+		if (typeof content === "string") {
+			return <p>{content}</p>;
+		} else {
+			return (
+				<ul>
+					{content.map((item) => (
+						<li className="collapse__list" key={item}>
+							{item}
+						</li>
+					))}
+				</ul>
+			);
+		}
+	}
+
+	render() {
+		return (
+			<div className="collapse">
+				<div
+					className="collapse__visible"
+					onClick={() => {
+						this.toggleCollapse();
+					}}
+				>
+					<h2 class="collapse__visible__title">{this.props.title}</h2>
+					<img
+						src={Chevron}
+						alt="chevron down"
+						className={
+							this.state.isOpenned ? "collapse__visible__chevron rotate" : "collapse__visible__chevron"
+						}
+					></img>
+				</div>
+
+				<div
+					className={this.state.isOpenned ? "collapse__toggle animated" : "collapse__toggle"}
+					style={{ height: this.state.isOpenned ? `auto` : "0px" }}
+				>
+					<p className="collapse__toggle__content">{this.collapseContent()}</p>
+				</div>
+			</div>
+		);
+	}
+}
