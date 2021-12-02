145ccbae15ea34235dad15ec8039a8a379090558
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-02 18:45:54 +0100

    ⚡️  Improve import and stuff

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import { Routes, Route } from "react-router-dom";
 import About from "./Containers/About/About";
 import Error from "./Containers/Errors/Error";
 import Home from "./Containers/Home/Home";
+import "./Containers/Home/Home.scss";
 import { Component } from "react";
 import Rental from "./Containers/Rental/Rental";
 import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
 
 export default class App extends Component {
 	render() {
-
 		return (
 			<>
-			<Navbar></Navbar>
+				<Navbar className="homepage"></Navbar>
 				<Routes>
 					<Route path="*" element={<Error />}></Route>
 					<Route path="/about" element={<About />}></Route>
 					<Route path="/rental/:id" element={<Rental />}></Route>
 					<Route path="/" element={<Home />}></Route>
 				</Routes>
+				<Footer className="footer" />
 			</>
 		);
 	}
