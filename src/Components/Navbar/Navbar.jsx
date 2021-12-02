bb358a66fead5e01f294cabe481d1f5f96bc29bd
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-02 18:46:39 +0100

    📱  Fix responsiveness

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,13 +7,13 @@ import React, { Component } from "react";
 export default class Navbar extends Component {
 	render() {
 		return (
-			<>
+			<div className='navbar'>
 				<Logo className="navbar__logo"></Logo>
 				<nav className="navbar__nav">
 					<NavLink className={({isActive}) => (isActive ? "navbar__nav__link active" : "navbar__nav__link")} to="/">Accueil</NavLink>
      <NavLink className={({isActive}) => (isActive ? "navbar__nav__link active" : "navbar__nav__link")} to="/about">A propos</NavLink>
 				</nav>
-			</>
+			</div>
 		);
 	}
 }
