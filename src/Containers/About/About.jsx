c3d959234f3260ebae76444a128e6ef13353fd4f
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-02 18:45:29 +0100

    ✨ Update about page

--- src/Containers/About/About.jsx
+++ src/Containers/About/About.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
+import Collapse from "../../Components/Collapse/Collapse";
+import Herobanner from "../../Components/Herobanner/Herobanner";
+import guidelines from "../../data/guidelines.json";
+import "./About.scss";
 
 export default class About extends Component {
 	render() {
 		return (
-			<div>
-				<h1>C'est a propos</h1>
+			<div className="about">
+				<Herobanner origin="about"></Herobanner>
+				<section className="about__guidelines">
+					{guidelines.map((guideline) => {
+						return (
+							<Collapse title={guideline.title} key={guideline.id} content={guideline.description} />
+						);
+					})}
+				</section>
 			</div>
 		);
 	}
