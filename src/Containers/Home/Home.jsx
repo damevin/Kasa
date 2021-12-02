3918e10b5ab75b0d54690757d27dd45bd3f09cef
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-02 18:45:17 +0100

    ✨ Finish homepage

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
 import Herobanner from "../../Components/Herobanner/Herobanner";
 import RentalCard from "../../Components/RentalCard/RentalCard";
+import "./Home.scss";
 import data from "../../data/data.json";
-import { useState } from "react";
 
 export default class Home extends Component {
 	render() {
 		const dataArr = data;
+		console.log(dataArr);
 		return (
-			<main>
-				<Herobanner></Herobanner>
-				<section class="rental__section">
+			<main className="homepage">
+				<Herobanner origin="homepage"></Herobanner>
+
+				<section className="rental__section">
 					{dataArr.map((item, index) => {
-						return <RentalCard title={item.title} cover={item.cover} key={index} id={item.id}></RentalCard>;
+						return (
+							<RentalCard title={item.title} cover={item.cover} key={index} id={item.id}></RentalCard>
+						);
 					})}
 				</section>
 			</main>
