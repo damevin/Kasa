9b8f141eb902192527e304f11748678e2a5f0fb0
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-02 13:44:58 +0100

    🎨  Import RentalCard, update UI

--- src/Components/RentalCard/RentalCard.jsx
+++ src/Components/RentalCard/RentalCard.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import "./RentalCard.scss";
+import Collapse from "../Collapse/Collapse";
 
 export default class RentalCard extends Component {
 	render() {
--- src/Containers/Rental/Rental.jsx
+++ src/Containers/Rental/Rental.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import data from "../../data/data.json";
+import Collapse from "../../Components/Collapse/Collapse.jsx";
 
 export default class Rental extends Component {
 	render() {
 		const rentalData = data;
 		const rentalUrlId = window.location.pathname.substr(8);
 		const rental = rentalData.find((item) => item.id === rentalUrlId);
-  
-		return <div>{rental.title}</div>;
+	
+		return (
+			<div>
+				{rental.title}
+				<Collapse title="Équipements" content={rental.equipments}></Collapse>
+				<Collapse title="Description" content={rental.description}></Collapse>
+			</div>
+		);
 	}
 }
