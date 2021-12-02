d6b292339f1ba7fe920f73c6c8988411b51ceb42
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-02 18:47:10 +0100

    ✨  Create component for see host informations

---
+++ src/Components/RentalHeader/RentalHost/RentalHost.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./RentalHost.scss";
+
+export default class RentalHost extends Component {
+	render() {
+		return (
+			<div className="host">
+				<p className="host__name">{this.props.host.name}</p>
+				<img className="host__img" src={this.props.host.picture} alt=""></img>
+			</div>
+		);
+	}
+}
