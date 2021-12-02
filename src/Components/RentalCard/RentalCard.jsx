ae28607640a95feb5c3ef50cad122dc364a6e88e
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-02 18:46:54 +0100

    💫  Work on hover animation

--- src/Components/RentalCard/RentalCard.jsx
+++ src/Components/RentalCard/RentalCard.jsx
@@ -1,17 +1,15 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import Collapse from "../Collapse/Collapse";
+import './RentalCard.scss';
 
 export default class RentalCard extends Component {
 	render() {
 
 		return (
-			<Link to={"/rental/" + this.props.id}>
-				<article className="rentalcard">
+			<Link className="rentalcard" to={"/rental/" + this.props.id}>
 					<div className="rentalcard__overlay"></div>
 					<img className="rentalcard__img" src={this.props.cover} alt=""></img>
-					<h2 className="rentalcard__title">{this.props.title}</h2>s
-				</article>
+					<h2 className="rentalcard__title">{this.props.title}</h2>
 			</Link>
 		);
 	}
